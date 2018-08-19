140606400bea212fc4048b4e5c846734ff182409
Tharles Amaro <tharlesamaro@gmail.com>
2018-08-19 01:09:07 -0300

    Iniciando CRUD do usuário.

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use App\Http\Requests\UserRequest;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
+    }
+
+    public function new()
+    {
+        return view('admin.users.store');
+    }
+
+    public function store(UserRequest $request)
+    {
+        $userData = $request->all();
+
+        $request->validated();
+
+        $user = new User();
+        $user->create($userData);
+
+        print 'Usuário criado com sucesso!';
+    }
+
+    public function edit(User $user)
+    {
+        return view('admin.users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, $id)
+    {
+        $userData = $request->all();
+
+        $request->validated();
+
+        $user = User::findOrFail($id);
+        $user->update($userData);
+
+        print 'Usuário atualizado com sucesso';
+    }
+
+    public function delete($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        print 'Usuário deletado com sucesso';
+    }
+}
--- resources/views/admin/restaurant/edit.blade.php
+++ resources/views/admin/restaurant/edit.blade.php
@@ -9,7 +9,7 @@
         {{ csrf_field() }}
 
         <p class="form-group">
-            <label for="name">Nome do restaurante:</label> <br>
+            <label for="name">Nome do restaurante:</label>
             <input type="text" name="name" id="name" value="{{ $restaurant->name }}"
                    class="form-control @if($errors->has('name')) is-invalid @endif">
 
@@ -21,7 +21,7 @@
         </p>
 
         <p class="form-group">
-            <label for="address">Endereço:</label> <br>
+            <label for="address">Endereço:</label>
             <input type="text" name="address" id="address" value="{{ $restaurant->address }}"
                    class="form-control @if($errors->has('address')) is-invalid @endif">
 
@@ -33,7 +33,7 @@
         </p>
 
         <p class="form-group">
-            <label for="description">Fale sobre o restaurante:</label> <br>
+            <label for="description">Fale sobre o restaurante:</label>
             <textarea name="description" id="description" cols="30" rows="10"
                       class="form-control @if($errors->has('description')) is-invalid @endif">{{ $restaurant->description }}</textarea>
 
